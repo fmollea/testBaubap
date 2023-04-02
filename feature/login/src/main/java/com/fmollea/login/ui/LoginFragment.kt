7ad36d41c583049f5db086f5eeb0ede8e7151c25
fede-mollea <fedemollea@platzi.com>
2023-04-02 11:31:54 -0300

    added navigation between fragment and dialog

--- feature/login/src/main/java/com/fmollea/login/ui/LoginFragment.kt
+++ feature/login/src/main/java/com/fmollea/login/ui/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.fmollea.login.R
 import com.fmollea.login.databinding.FragmentLoginBinding
 import com.fmollea.login.viewmodel.LoginViewModel
@@ -29,18 +30,23 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             binding.txtPassword.editText?.text.toString()).observe(viewLifecycleOwner) { result ->
             when(result) {
                 is LoginViewModel.LoginViewState.EmptyOrNullFields -> {
-
+                    navToLoginDialog(getString(R.string.empty_or_null_fields))
                 }
                 is LoginViewModel.LoginViewState.InvalidEmail -> {
-
+                    navToLoginDialog(getString(R.string.invalid_email))
                 }
                 is LoginViewModel.LoginViewState.Success -> {
-
+                    navToLoginDialog(getString(R.string.success))
                 }
                 is LoginViewModel.LoginViewState.Error -> {
-
+                    navToLoginDialog(getString(R.string.error))
                 }
             }
         }
+    }
+
+    private fun navToLoginDialog(message: String) {
+        val action = LoginFragmentDirections.actionLoginFragmentToMessageLoginDialogFragment(message)
+        findNavController().navigate(action)
     }
 }
--- feature/login/src/main/java/com/fmollea/login/ui/MessageLoginDialogFragment.kt
+++ feature/login/src/main/java/com/fmollea/login/ui/MessageLoginDialogFragment.kt
@@ -1,8 +1,22 @@
 package com.fmollea.login.ui
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
 import com.fmollea.login.R
+import com.fmollea.login.databinding.FragmentMessageLoginDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class MessageLoginDialogFragment : Fragment(R.layout.fragment_message_login_dialog) {
+class MessageLoginDialogFragment : BottomSheetDialogFragment(R.layout.fragment_message_login_dialog) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentMessageLoginDialogBinding.bind(view)
+        val args: MessageLoginDialogFragmentArgs by navArgs()
+
+        binding.tvMessage.text = args.message
+    }
 }
+
+
