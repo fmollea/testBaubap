1e882b5486b215b6e8be55bca8dab3712bb66794
fede-mollea <fedemollea@platzi.com>
2023-04-02 09:30:25 -0300

    created modules

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,64 @@
+object Versions {
+    const val core = "1.9.0"
+    const val appcompat = "1.6.1"
+    const val androidMaterial = "1.8.0"
+    const val constraintLayout = "2.1.4"
+
+    const val testImplJunit = "4.13.2"
+    const val androidTestImplJunit = "1.1.5"
+    const val androidTestEspresso = "3.5.1"
+
+    const val kotlinCoroutines = "1.6.1"
+
+    const val coroutineLifecycleScope = "2.5.1"
+
+    const val viewModel = "2.6.1"
+
+    const val hilt = "2.44"
+    const val hiltCompiler = "1.0.0"
+
+
+    const val navigation = "2.5.3"
+
+    const val swipeRefresh = "1.1.0"
+}
+
+object Deps {
+    const val core = "androidx.core:core-ktx:${Versions.core}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val androidMaterial = "com.google.android.material:material:${Versions.androidMaterial}"
+    const val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+}
+
+object TestImplementation {
+    const val junit = "junit:junit:${Versions.testImplJunit}"
+}
+
+object AndroidTestImplementation {
+    const val junit = "androidx.test.ext:junit:${Versions.androidTestImplJunit}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.androidTestEspresso}"
+}
+
+
+object Navigation {
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+    const val navigation = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    const val navigationDynamic = "androidx.navigation:navigation-dynamic-features-fragment:${Versions.navigation}"
+}
+
+object ViewModel {
+    const val viewModel = "androidx.activity:activity-ktx:${Versions.viewModel}"
+    const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.viewModel}"
+}
+
+object Hilt {
+    const val hilt = "com.google.dagger:hilt-android:${Versions.hilt}"
+    const val hiltAndroidCompiler = "com.google.dagger:hilt-android-compiler:${Versions.hilt}"
+    const val hiltCompiler = "androidx.hilt:hilt-compiler:${Versions.hiltCompiler}"
+}
+
+object CircularProgressBar {
+    const val swipeRefresh =
+        "androidx.swiperefreshlayout:swiperefreshlayout:${Versions.swipeRefresh}"
+}
